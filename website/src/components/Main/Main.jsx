89342ef0e1f1fa5c97fb211410e721effc9e1432
Simon <simonfong6@gmail.com>
2020-05-27 18:36:01 -0700

    Add Main empty CSS file

--- website/src/components/Main/Main.jsx
+++ website/src/components/Main/Main.jsx
@@ -8,6 +8,8 @@ import Header from '../Header';
 import SubtleAsianDating from '../SubtleAsianDating';
 import MoreProjects from '../MoreProjects';
 
+import './Main.css';
+
 function Main() {
   return (
     <div style={{ textAlign: 'center' }}>
