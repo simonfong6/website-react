58d156d1a0b9fc4ca5499483cf88d76778f6c100
Simon <simonfong6@gmail.com>
2020-05-27 17:48:44 -0700

    Create Header component

---
+++ website/src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import SocialIcon from '../SocialIcon';
+
+const data = {
+  icons: [
+    {
+      icon: 'envelope-square',
+      link: 'mailto:simonfong6@gmail.com',
+      tooltip: 'Email',
+    },
+    {
+      icon: 'github-square',
+      link: 'https://github.com/simonfong6',
+      tooltip: 'GitHub',
+    },
+    {
+      icon: 'linkedin-square',
+      link: 'https://linkedin.com/in/simonfong6',
+      tooltip: 'LinkedIn',
+    },
+    {
+      icon: 'file',
+      link: '/resume.pdf',
+      tooltip: 'Resume',
+    },
+  ],
+};
+
+function Header() {
+  const { icons } = data;
+
+  return (
+    <div>
+      <h1>Simon Fong</h1>
+      {
+        icons.map((icon) => (
+          <SocialIcon
+            icon={icon.icon}
+            link={icon.link}
+            tooltip={icon.tooltip}
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+export default Header;
--- website/src/components/Main.jsx
+++ website/src/components/Main.jsx
@@ -5,34 +5,14 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 
 // Custom Components
-import SocialIcon from './SocialIcon';
+import Header from './Header';
 import SubtleAsianDating from './SubtleAsianDating';
 
 function Main() {
   return (
     <div style={{ textAlign: 'center' }}>
       <Container>
-        <h1>Simon Fong</h1>
-        <SocialIcon
-          icon="envelope-square"
-          link="mailto:simonfong6@gmail.com"
-          tooltip="Email"
-        />
-        <SocialIcon
-          icon="github-square"
-          link="https://github.com/simonfong6"
-          tooltip="GitHub"
-        />
-        <SocialIcon
-          icon="linkedin-square"
-          link="https://linkedin.com/in/simonfong6"
-          tooltip="LinkedIn"
-        />
-        <SocialIcon
-          icon="file"
-          link="/resume.pdf"
-          tooltip="Resume"
-        />
+        <Header />
         <div className="card-deck">
           <SubtleAsianDating />
           <Card style={{ width: '18rem' }}>
