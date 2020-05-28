db58953a508eb1535ef30c5ab8d7725031fac5bc
Simon <simonfong6@gmail.com>
2020-05-27 17:29:46 -0700

    Move Listgroups into data

--- website/src/components/SubtleAsianDating/SubtleAsianDating.jsx
+++ website/src/components/SubtleAsianDating/SubtleAsianDating.jsx
@@ -9,33 +9,65 @@ import SocialIcon from '../SocialIcon';
 
 const dataSciencePresentationUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vS6ga2yJyG4DLHy3NqM45bMSaWShGB8QL8UWTp_AIHuywmi9bT1lAvE9VftSEF6QdJfw8IVJLOL5IXH/pub?start=false&loop=false&delayms=3000&slide=id.p1';
 
+const data = {
+  title: 'Subtle Asian Dating Score Test',
+  description: 'A quiz to measure how dateable you are in the Subtle Asian Dating community.',
+  details: [
+    {
+      type: 'text',
+      text: 'Built within 4 hours',
+    },
+    {
+      type: 'text',
+      text: 'Over 20,000 visits',
+    },
+    {
+      type: 'link',
+      text: 'Data Analysis Presentation',
+      link: dataSciencePresentationUrl,
+    },
+    {
+      type: 'link',
+      text: 'Data Analysis Code',
+      link: 'https://github.com/simonfong6/ECE-143-Team-6',
+    },
+  ],
+};
+
 function SubtleAsianDating() {
+  const {title, description, details} = data;
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>Subtle Asian Dating Score Test</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-          A quiz to measure how dateable you are in the Subtle Asian Dating
-          community.
+          {description}
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroupItem>
-          Built within 4 hours
-        </ListGroupItem>
-        <ListGroupItem>
-          Over 20,000 visits
-        </ListGroupItem>
-        <ListGroupItem>
-          <Card.Link href={dataSciencePresentationUrl}>
-            Data Analysis Presentation
-          </Card.Link>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Card.Link href="https://github.com/simonfong6/ECE-143-Team-6">
-            Data Analysis Code
-          </Card.Link>
-        </ListGroupItem>
+        {
+          details.map((detail) => {
+            switch (detail.type) {
+              case 'text':
+                return (
+                  <ListGroupItem>
+                    {detail.text}
+                  </ListGroupItem>
+                );
+              case 'link':
+                return (
+                  <ListGroupItem>
+                    <Card.Link href={detail.link}>
+                      {detail.text}
+                    </Card.Link>
+                  </ListGroupItem>
+                );
+              default:
+                return null;
+            }
+          })
+        }
       </ListGroup>
       <Card.Body>
         <SocialIcon
